5c28795976d32a12a6a0667dbe5fcfadb027847e
E. Kevin Hall <hall@absinthe.org>
2015-06-24 18:14:54 -0400

    Initial Commit

---
+++ ScrollviewOffsetTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollviewOffsetTest
+//
+//  Created by E. Kevin Hall on 6/24/15.
+//  Copyright (c) 2015 E. Kevin Hall, Yale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
